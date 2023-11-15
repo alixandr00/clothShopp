d6e99b94e43ddc1fc621c76c6f99c3050214463f
Ali <alisamatov106@gmail.com>
2023-11-15 16:46:34 +0600

    no finished Main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Main = () => {
+  const [openMainModal, setOpenMainModal] = useState(false);
   return <div>Main</div>;
 };
---
+++ src/components/main/MainModal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { UiModal } from "../Ui/modal/UiModal";
+
+export const MainModal = () => {
+  return (
+    <UiModal>
+      <div></div>
+    </UiModal>
+  );
+};
