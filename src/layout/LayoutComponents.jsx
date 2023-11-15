a340154d9bab8e35ba33e44b73da2b003d733365
Ali <alisamatov106@gmail.com>
2023-11-15 16:04:08 +0600

    created sidebar

--- src/layout/LayoutComponents.jsx
+++ src/layout/LayoutComponents.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../components/header/Header";
+import { Sidebar } from "../components/sidebar/Sidebar";
 
 export const LayoutComponents = () => {
   return (
     <div>
       <Header />
+      <Sidebar />
     </div>
   );
 };
