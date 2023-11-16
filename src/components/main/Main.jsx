ebc05545a551cc1bb10500e3334d875975ad3416
Ali <alisamatov106@gmail.com>
2023-11-16 14:24:40 +0600

    no finished clothes

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,30 @@
 import React, { useState } from "react";
+import { MainModal } from "./MainModal";
 
 export const Main = () => {
   const [openMainModal, setOpenMainModal] = useState(false);
-  return <div>Main</div>;
+
+  const openMainModalHandler = () => {
+    setOpenMainModal((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <button
+        className="ml-20 inline-block outline-none appearance-none px-3 rounded-4 no-underline bg-gray-200 border border-gray-500 shadow-md text-gray-700 text-base font-normal h-10 transition-all duration-150 ease-in-out hover:text-gray-700 hover:bg-white hover:border-gray-600 focus:outline-none focus:shadow-outline"
+        onClick={openMainModalHandler}
+      >
+        добавить одежду
+      </button>
+      {openMainModal && (
+        <>
+          <MainModal openMainModalHandler={openMainModalHandler} />
+          <div
+            onClick={openMainModalHandler}
+            className="fixed w-screen h-[100vh] top-0 z-[2]"
+          />
+        </>
+      )}
+    </div>
+  );
 };
--- src/components/main/MainModal.jsx
+++ src/components/main/MainModal.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { UiModal } from "../Ui/modal/UiModal";
+import ExitIcon from "../../assets/icons/logout-svgrepo-com.svg";
 
-export const MainModal = () => {
+export const MainModal = ({ openMainModalHandler }) => {
   return (
-    <UiModal>
-      <div></div>
-    </UiModal>
+    <div className="fixed top-0 flex justify-center items-center w-screen h-screen z-10">
+      <div className="w-96 h-[60vh] bg-gray-400 rounded-md p-4 relative">
+        <div className="w-full flex justify-end">
+          <img
+            onClick={openMainModalHandler}
+            className="cursor-pointer"
+            src={ExitIcon}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
   );
 };
